5ce923992f526fffe98777e39602ba60c1c25dca
etudiant <etudiant@DepInfo-XXX>
2021-10-15 20:41:18 -0400

    Ajout des interfaces fournient avec le lab

--- LabDecorateur/src/labdecorateur/Interfaces/ICommande.java
+++ LabDecorateur/src/labdecorateur/Interfaces/ICommande.java
@@ -3,20 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package labdecorateur;
+package labdecorateur.Interfaces;
 
 /**
  *
  * @author etudiant
  */
-public class LabDecorateur {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-        
-    }
-    
+public interface ICommande 
+{
+    void executer(ISortie sortie);
 }
